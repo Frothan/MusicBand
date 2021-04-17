51ae684ecb2c408b8d35a535ab6f3d67fa396bd1
julee99xd <julia.anderegg99@gmail.com>
2021-04-17 14:27:09 +0200

    tried to do the method getGenre... failed

--- music/src/MusicInstrument.java
+++ music/src/MusicInstrument.java
@@ -1,10 +1,27 @@
+import java.io.*;
+
 abstract class MusicInstrument {
+
+    String[] genres = {"Rock","Blues","Klassik","Pop","Country",
+                       "HipHop","Jazz","Metal","Techno"};
+
+    //Variable die von User eingegeben werden muss:
+    public String i = null;
 
     //not sure if return type is correct...
     public abstract void play();
     public abstract void setVolume();
     public abstract void getName();
     public abstract void getPlayTime();
-    public abstract void getGenre();
+
+    System.out.println(genres[1]);
+    System.out.println(parameter)
+    //public String getGenre(i){
+     //   genres[i];
+    //}
+
+
+
+
     public abstract void stopPlay();
 }
