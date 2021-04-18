fbd98772f319800cfea3f099c1af08220437a2c7
chr1st0jr <78206939+chr1st0jr@users.noreply.github.com>
2021-04-18 18:03:48 +0200

    JAR file included

--- music/src/iMusicInstrument.java
+++ music/src/iMusicInstrument.java
@@ -1,4 +1,9 @@
+import javazoom.jl.player.advanced.AdvancedPlayer;
+
 public interface iMusicInstrument {
+
+    AdvancedPlayer myAccordion = new AdvancedPlayer();
+
     void play();
     int setVolume(String instrument);
     void getName();
