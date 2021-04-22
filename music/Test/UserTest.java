4799c33f6eb7ded4fb33c7977ae8a505c094d2e0
julee99xd <julia.anderegg99@gmail.com>
2021-04-22 15:30:21 +0200

    junit integration

---
+++ music/Test/UserTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @org.junit.jupiter.api.Test
+    void userStart() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void whichInstrument() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void whichGenre() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void whichOption() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void callPlay() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void whichVolume() {
+    }
+}
